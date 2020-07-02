3b07901e5ef00f12b6e56d4d926612ee9ee99de8
Lucas <lucasgutierrez85@gmail.com>
2020-07-01 21:50:23 -0300

    create crud

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import UserTable from './Table/UserTable'
+import AddUserForm from './Table/AddUserForm'
+import EditUserForm from './EditUserForm'
 
-function App() {
+const App = () => {
+
+  const userData = [
+    { id : 1, name : 'Tania', username : 'sarasa'},
+    { id : 2, name : 'Tania2', username : 'sarasa2'},
+    { id : 3, name : 'Tania3', username : 'sarasa3'}
+  ]
+
+  const initialFormState = { id: null, name: '', username: '' }
+
+  const[editing, setEditing] = useState(false);
+
+  const [currentUser, setCurrentUser] = useState(initialFormState)
+
+  const [users, setUsers] = useState(userData);
+
+  const addUser = (user) => {
+    user.id = users.length + 1
+    setUsers([...users, user])
+  }
+
+  const deleteUser = id => (
+    setUsers(users.filter(user => user.id !== id))
+  )
+
+  const editUser = user => {
+    setEditing(true)
+
+    setCurrentUser({
+      id : user.id,
+      name : user.name,
+      username : user.username
+    })
+  }
+
+  const updateUser = (id, updateUser) => {
+    setEditing(false)
+
+    setUsers(users.map(u => (u.id === id ? updateUser : u)))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+        {
+          editing ? (
+            <div>
+              <h2>Edit user</h2>
+              <EditUserForm
+                setEditing={setEditing}
+                currentUser={currentUser}
+                updateUser={updateUser}
+              />
+            </div>
+          ) : (
+            <div>
+              <h2>Add user</h2>
+              <AddUserForm addUser={addUser} />
+            </div>
+          )
+        }
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTable users={users} delete={deleteUser} edit={editUser}/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Table/AddUserForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+const AddUserForm = (props) => {
+
+    const initialFormState = {
+        id : null,
+        name : '',
+        username : ''
+    };
+
+    const [user, setUser] = useState(initialFormState);
+
+    const handleInputChange = e => {
+        const { name, value } = e.target
+
+        setUser({...user, [name] : value})
+    }
+
+    return ( 
+        <form
+            onSubmit={ev => {
+                ev.preventDefault()
+
+                if(!user.name || !user.username) return
+
+                props.addUser(user)
+                setUser(initialFormState)
+            }}
+        >
+            <label>Name</label>
+            <input type="text" name="name" value={user.name} onChange={handleInputChange} />
+            <label>Username</label>
+            <input type="text" name="username" value={user.username} onChange={handleInputChange} />
+            <button>Add new user</button>
+        </form>
+     );
+}
+ 
+export default AddUserForm;
